aa541612327f3f45cf950b753a2b4589510fa087
arjunpnair7 <arjun.pnair7@gmail.com>
2024-03-18 10:21:25 +0530

    Another attempted fix

--- voting-app/src/app/page.tsx
+++ voting-app/src/app/page.tsx
@@ -21,8 +21,9 @@ export default function Home() {
   const handleCreateNewPartyClick = async() => {
     //api request, code
     console.log("LINE 15");
-    // const apiUrl = 'http://localhost:8080/new-party';
-    const apiUrl = 'https://food-app-backend-izbg.onrender.com/new-party'
+    const apiUrl = 'http://localhost:8080/new-party';
+    //https://food-app-backend-izbg.onrender.com
+    // const apiUrl = 'https://food-app-backend-izbg.onrender.com/new-party'
     fetch(apiUrl)
       .then((response) => {
         if (!response.ok) {
